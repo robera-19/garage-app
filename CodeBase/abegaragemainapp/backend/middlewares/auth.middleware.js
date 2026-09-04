053d07ee5ff2abf6acfca3038db342b7d197969e
Robera Wakjira <robarobera50@gmail.com>
2026-09-04 22:15:18 +0300

    implement the authentication middleware

---
+++ CodeBase/abegaragemainapp/backend/middlewares/auth.middleware.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const employeeService = require('../services/employee.service');
+
+// A function to verify the token received from the frontend
+const verifyToken = async (req, res, next) => {
+
+  const token = req.headers['x-access-token'];
+
+  if (!token) {
+    console.log('❌ NO TOKEN');
+
+    return res.status(403).send({
+      status: 'fail',
+      message: 'No token provided!',
+    });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+
+    if (err) {
+      return res.status(401).send({
+        status: 'fail',
+        message: 'Unauthorized!',
+      });
+    }
+
+    req.employee_id = decoded.employee_id;
+    req.employee_email = decoded.employee_email;
+
+    next();
+  });
+};
+
+// A function to check if the user is an admin
+const isAdmin = async (req, res, next) => {
+  // let token = req.headers["x-access-token"];
+  const employee_email = req.employee_email;
+  const employee = await employeeService.getEmployeeByEmail(employee_email);
+  if (employee[0].company_role_id === 3) {
+    next();
+  } else {
+    return res.status(403).send({
+      status: 'fail',
+      error: 'Not an Admin!',
+    });
+  }
+};
+
+const authMiddleware = {
+  verifyToken,
+  isAdmin,
+};
+
+module.exports = authMiddleware;
